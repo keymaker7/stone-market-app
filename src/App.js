3654d6654032fe15051237fdaa37a2d286f0e04e
keymaker7 <jkeymaker@gmail.com>
2025-08-07 00:34:51 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,3 @@
+// Supabase 설정 (본인 프로젝트 정보로 교체)
+const supabaseUrl = 'https://hrelytsjbrotkwltmign.supabase.co';  // ← 실제 URL로 변경
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZWx5dHNqYnJvdGt3bHRtaWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0OTEzMDAsImV4cCI6MjA3MDA2NzMwMH0.JY_uI4DcMQ80XrvPtORCdfWyGTDuY5YnsAxPQCuW9WU';  // ← 실제 키로 변경
